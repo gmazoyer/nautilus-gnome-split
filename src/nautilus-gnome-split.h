0e4139932dfb605e0f373e273efd67614b9fa073
Guillaume Mazoyer <respawneral@gmail.com>
2012-01-22 04:16:20 +0100

    Add licence header to the source files.

--- src/nautilus-gnome-split.h
+++ src/nautilus-gnome-split.h
@@ -1,3 +1,24 @@
+/*
+ * nautilus-gnome-split.h
+ * 
+ * Copyright (c) 2011 Guillaume Mazoyer
+ * 
+ * This file is part of GNOME Split Nautilus extension.
+ * 
+ * GNOME Split Nautilus extension is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * GNOME Split Nautilus extension is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with GNOME Split Nautilus extension.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 #ifndef NAUTILUS_GNOME_SPLIT_H
 #define NAUTILUS_GNOME_SPLIT_H
 
